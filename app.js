7dc92452683c73c603c96674491878acf937c624
marineco <marinecolonge@gmail.com>
2016-10-17 15:43:06 +0200

    bug boutons à résoudre

--- app.js
+++ app.js
@@ -1,30 +1,69 @@
-(function() {
+$(document).ready(function(){
 
-	var t = 1500;
-	var interval;
+	window.app = {
+		t: 1500,
+		intervalID: null,
+		init: function(){
+			app.listeners();
+		},
+		listeners: function(){
+			$('#pomodoro').on('click', app.pomodoro);
+			$('#shortBreak').on('click', app.shortBreak);
+			$('#longBreak').on('click', app.longBreak);
+			$('start').on('click', app.start);
+			$('stop').on('click', app.stop);
+			$('reset').on('click', app.reset);
+		},
+		start: function(){
+			app.intervalID = setInterval(function(){
+				app.updateView();
+				app.t--;
+				if (app.t === 0) {
+   					app.stop();
+   				}
+			}, 1000);
+		},
+		updateView: function(min, sec){
+			var minutes = Math.floor(app.t/60);
+			var secondes = app.t % 60;
+			if (minutes < 10 && secondes < 10) {
+   				$('h1').html('0' + minutes + ':' + '0' + secondes);
+   			}
+   			else if (minutes < 10 && secondes >= 10) {
+   				$('h1').html('0' + minutes + ':' + secondes);
+   			}
+   			else if (minutes >= 10 && secondes < 10) {
+   				$('h1').html(minutes + ':' + '0' + secondes);
+   			}
+   			else {
+   				$('h1').html(minutes + ':' + secondes);
+   			}
+		},
+		stop: function(){
+			clearInterval(app.intervalID);
+		},
+		reset: function(){
+			clearInterval(app.intervalID);
+			app.t;
+			app.updateView();
+		},
+		pomodoro: function(){
+			app.t = 1500;
+			app.updateView();
+		},
 
-	$('h1').text('25:00');
+		shortBreak: function(){
+			app.t = 300;
+			app.updateView();
+		},
 
-	$('#start').on('click', function(){
-		interval = setInterval(function() {
-		t--;
-		minutes = Math.floor(t/60);
-		secondes = t % 60;
+		longBreak: function(){
+			app.t = 600;
+			app.updateView();
+		},
 
-		$('h1').text(minutes + ':' + secondes);
+	};
 
-		}, 1000);
-	});
+	app.init();
 
-	$('#stop').on('click', function(){
-		clearInterval(interval);
-	});
-
-	$('#reset').on('click', function(){
-		clearInterval(interval);
-		t = 1500;
-		$('h1').text('25' + ':' + '00');
-
-	});
-
-})();
+});
